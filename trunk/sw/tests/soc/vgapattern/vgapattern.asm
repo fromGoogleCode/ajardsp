2cb1cb6f34c9ae2fac323f8bfbe185714cb9407d
denis127@googlemail.com <denis127@googlemail.com@6097abb8-8f73-2d41-b057-73858f9c9e90>
2011-07-19 07:54:00 +0000

    upd with new asm instr

--- trunk/sw/tests/soc/vgapattern/vgapattern.asm
+++ trunk/sw/tests/soc/vgapattern/vgapattern.asm
@@ -22,32 +22,32 @@
 
 
         /* Start the VGA ctrl. */
-        ldimm16 $ptr1,  0xf001
-        ldimm16 $ptr0,  0x0004
-        ldimm16 $acc1h, 0x0000
-        ldimm16 $acc1l, 0x0000
-        stext32 $ptr0, $ptr1, $acc1
+        ldimm16 0xf001, $ptr1
+        ldimm16 0x0004, $ptr0
+        ldimm16 0x0000, $acc1h
+        ldimm16 0x0000, $acc1l
+        stext32 $acc1, $ptr0, $ptr1
 
-        ldimm16 $ptr1,  0xf001
-        ldimm16 $ptr0,  0x0000
-        ldimm16 $acc1l, 0x0001
-        stext32 $ptr0, $ptr1, $acc1
+        ldimm16 0xf001, $ptr1
+        ldimm16 0x0000, $ptr0
+        ldimm16 0x0001, $acc1l
+        stext32 $acc1, $ptr0, $ptr1
 
         /* Clear frame buffer */
-        ldimm16 $ptr2,  0x0008
-        ldimm16 $ptr1,  0x0000
-      | ldimm16 $ptr0,  0xfff0
+        ldimm16 0x0008, $ptr2
+        ldimm16 0x0000, $ptr1
+      | ldimm16 0xfff0, $ptr0
         mvts16 $ptr2, $bkrepcnt
-        ldimm16 $acc0l, 0x0000
-      | ldimm16 $acc0h, 0x0000
-        ldimm16 $acc1l, 0xfff0
-      | ldimm16 $acc1h, 0x0000
+        ldimm16 0x0000, $acc0l
+      | ldimm16 0x0000, $acc0h
+        ldimm16 0xfff0, $acc1l
+      | ldimm16 0x0000, $acc1h
 
         nop
         bkrep #loop_clear_end
         extburst 0x05, $ptr0, $ptr1, $acc0
         add32 $acc0, $acc1, $acc0
-        addptr16 $ptr1, 0x2
+        addptr16 0x2, $ptr1
 loop_clear_end:
         nop
         nop
