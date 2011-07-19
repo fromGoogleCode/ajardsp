c916ce8383f808bb8088ac2e9207b061d0d0e953
denis127@googlemail.com <denis127@googlemail.com@6097abb8-8f73-2d41-b057-73858f9c9e90>
2011-07-19 07:55:21 +0000

    upd with new asm instr

--- trunk/sw/tests/soc/adc2graph/adc2graph.asm
+++ trunk/sw/tests/soc/adc2graph/adc2graph.asm
@@ -12,35 +12,35 @@
         /* Start the VGA ctrl. */
 
         xor32 $acc1, $acc1, $acc1
-        ldimm16 $ptr1,  0xf001
-        ldimm16 $ptr0,  0x0004
-        ldimm16 $acc1h, 0x0000
-        ldimm16 $acc1l, 0x0000
+        ldimm16 0xf001, $ptr1
+        ldimm16 0x0004, $ptr0
+        ldimm16 0x0000, $acc1h
+        ldimm16 0x0000, $acc1l
         nop
         nop
-        stext32 $ptr0, $ptr1, $acc1
+        stext32 $acc1, $ptr0, $ptr1
 
-        ldimm16 $ptr1,  0xf001
-        ldimm16 $ptr0,  0x0000
-        ldimm16 $acc1l, 0x0001
+        ldimm16 0xf001, $ptr1
+        ldimm16 0x0000, $ptr0
+        ldimm16 0x0001, $acc1l
         nop
         nop
-        stext32 $ptr0, $ptr1, $acc1
+        stext32 $acc1, $ptr0, $ptr1
 
-        xor32 $acc0, $acc0, $acc0
-        ldimm16 $ptr0,  0x0000
+        xor32   $acc0, $acc0, $acc0
+        ldimm16 0x0000,$ptr0
         nop
-        st32 $ptr0, $acc0
+        st32    $acc0, $ptr0
 
         /* Clear frame buffer */
-        ldimm16 $ptr2,  0x0008
-        ldimm16 $ptr1,  0x0000
-      | ldimm16 $ptr0,  0xfff0
+        ldimm16 0x0008, $ptr2
+        ldimm16 0x0000, $ptr1
+      | ldimm16 0xfff0, $ptr0
         mvts16 $ptr2, $bkrepcnt
-        ldimm16 $acc0l, 0x0000
-      | ldimm16 $acc0h, 0x0000
-        ldimm16 $acc1l, 0xfff0
-      | ldimm16 $acc1h, 0x0000
+        ldimm16 0x0000, $acc0l
+      | ldimm16 0x0000, $acc0h
+        ldimm16 0xfff0, $acc1l
+      | ldimm16 0x0000, $acc1h
 
         nop
         bkrep #loop_clear_end
@@ -53,28 +53,28 @@ loop_clear_end:
 forever:
 
         /* Read from ADC */
-        ldimm16 $acc0l, 0x0000
-      | ldimm16 $acc0h, 0xe000
-        ldimm16 $ptr0,  200
-      | ldimm16 $ptr1,  0x0000
+        ldimm16 0x0000, $acc0l
+      | ldimm16 0xe000, $acc0h
+        ldimm16 200,    $ptr0
+      | ldimm16 0x0000, $ptr1
 
         extburst 0x02, $ptr0, $ptr1, $acc0
 
         /* Write the graph */
-        ldimm16 $acc7l, 0xffff
-      | ldimm16 $acc7h, 0xffff
-        ldimm16 $ptr0,  200
-      | ldimm16 $acc1l, 200  /* should be 800 (screen width) but we want to divide the samples by 4 */
-        ldimm16 $acc2l, 0
-      | ldimm16 $acc2h, 0
-        ldimm16 $ptr4,  0
+        ldimm16 0xffff, $acc7l
+      | ldimm16 0xffff, $acc7h
+        ldimm16 200,    $ptr0
+      | ldimm16 200,    $acc1l  /* should be 800 (screen width) but we want to divide the samples by 4 */
+        ldimm16 0,      $acc2l
+      | ldimm16 0,      $acc2h
+        ldimm16 0,      $ptr4
 
         mvts16 $ptr0, $bkrepcnt
         nop
         bkrep #loop_graph
         ldinc32 $ptr3, $acc0
         mv16  $ptr4, $acc2l
-      | addptr16 $ptr4, 4
+      | addptr16 4, $ptr4
         mpy16 $acc1l, $acc0l, $acc0
         nop
         add32 $acc0, $acc2, $acc0
@@ -82,7 +82,7 @@ forever:
 loop_graph:
         mv16 $acc0l, $ptr0
       | mv16 $acc0h, $ptr1
-        stext32 $ptr0, $ptr1, $acc7
+        stext32 $acc7, $ptr0, $ptr1
 
         bra #forever
 
